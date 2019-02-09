3c9c062aeac548ccb797c193b9fb71ade9e4adaf
HuguesRomain <huguesromainpro@gmail.com>
2019-02-09 18:23:55 +0100

    conditions

--- coursPHP/variable.php
+++ coursPHP/variable.php
@@ -1,6 +1,36 @@
 <?php 
+/* 
+$ageDuVisiteur = 20;
+echo 'Le visiteur a '.$ageDuVisiteur.' ans.';
 
-$prix = `bonjour`;
-echo $prix;
+$prix = 2.5;
+$quantite = 10;
+
+$total = $prix + $quantite; 
+
+echo 'cela coute'.$total.'$'; */
+/* 
+$age = 18;
+$adulte;
+
+if($age >= 18){
+$adulte = true;
+} else {
+$adulte = false;
+}
+
+if($adulte == true){
+  echo 'tu peu accedé au site'; 
+} else {
+  echo'désolé tu est trop jeune';
+} */
+
+$adulte = true;
+$nom = 'Bernard'; 
+
+if($adulte AND $nom == 'Bernard'){
+
+}
+
 
 ?>
