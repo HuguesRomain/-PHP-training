88e148d8d0081984ef0bfb4d911716c729ccdac5
HuguesRomain <huguesromainpro@gmail.com>
2019-02-09 18:56:30 +0100

    conditions

--- coursPHP/variable.php
+++ coursPHP/variable.php
@@ -1,5 +1,8 @@
 <?php 
 /* 
+
+// LES VARIABLES 
+
 $ageDuVisiteur = 20;
 echo 'Le visiteur a '.$ageDuVisiteur.' ans.';
 
@@ -13,6 +16,8 @@ echo 'cela coute'.$total.'$'; */
 $age = 18;
 $adulte;
 
+// LES CONDITIONS 
+
 if($age >= 18){
 $adulte = true;
 } else {
@@ -24,13 +29,34 @@ if($adulte == true){
 } else {
   echo'désolé tu est trop jeune';
 } */
-
+/* 
 $adulte = true;
-$nom = 'Bernard'; 
+$nom = 'Bernard';
 
 if($adulte AND $nom == 'Bernard'){
+} */
+/* 
+$age = 18; 
 
+switch($age){
+  case 4: 
+  echo'tu a 4 ans';
+  break;
+  case 16: 
+  echo'tu a 16 ans';
+  case 18: 
+  echo'tu est majeur'; 
+} */
+
+// LES BOUCLES 
+
+$repetitions = 0; 
+/* while($repetitions < 1000){
+  echo'<p>Je ne doit pas copier sur mon voisin.'.$repetitions.'</p>';     
+  $repetitions++;
+} */
+for($repetitions = 0; $repetitions < 10; $repetitions++){
+  echo'<p>Je ne doit pas copier sur mon voisin.'.$repetitions.'</p>';     
 }
-
 
 ?>
