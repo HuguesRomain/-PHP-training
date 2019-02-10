f0d11059d70b64456a25a83b0114911115d5d673
HuguesRomain <huguesromainpro@gmail.com>
2019-02-10 15:55:01 +0100

    function

--- coursPHP/cours.php
+++ coursPHP/cours.php
@@ -68,17 +68,77 @@ $prenom[2] = 'Robert';  */
 /* $prenom = array('Mathieu','Marie', 'Robert'); 
 echo print_r($prenom); */
 
-
-$prenoms = array('Mathieu','Marie', 'Robert'); 
-$persons = array('nom' => 'Marie', 'prenom' => 'Robert', 'age' => 10); 
-/* print_r($person); */
 /* 
+$prenoms = array('Mathieu','Marie', 'Robert'); 
+$persons = array(
+  'nom' => 'Marie', 
+  'prenom' => 'Robert', 
+  'age' => 10);
+
+
+print_r($person);
+
 for($numero = 0; $numero < 3; $numero++)
 {
  echo '<p>'.$prenom[$numero].'</p>';
-} */
+}
 foreach($persons as $libelle => $detail)
 {
  echo '<p>'.$libelle.' = '.$detail.'</p>';
 }
+
+$coordonnees = array (
+    'prenom' => 'François',
+    'nom' => 'Dupont',
+    'adresse' => '3 Rue du Paradis',
+    'ville' => 'Marseille');
+
+if (array_key_exists('nom', $coordonnees))
+{
+    echo 'La clé "nom" se trouve dans les coordonnées !';
+}
+
+if (array_key_exists('pays', $coordonnees))
+{
+    echo 'La clé "pays" se trouve dans les coordonnées !';
+}
+
+
+$fruits = array ('Banane', 'Pomme', 'Poire', 'Cerise', 'Fraise', 'Framboise');
+
+if (in_array('Myrtille', $fruits))
+{
+    echo 'La valeur "Myrtille" se trouve dans les fruits !';
+}
+
+if (in_array('Cerise', $fruits))
+{
+    echo 'La valeur "Cerise" se trouve dans les fruits !';
+}
+
+
+$fruits = array ('Banane', 'Pomme', 'Poire', 'Cerise', 'Fraise', 'Framboise');
+
+$position = array_search('Fraise', $fruits);
+echo '"Fraise" se trouve en position ' . $position . '<br />';
+
+$position = array_search('Banane', $fruits);
+echo '"Banane" se trouve en position ' . $position; */
+
+// LES FONCTIONS
+
+/* $phrase = 'Bonjour je suis une phrase un peu plus longue.';
+$nombreDeCaractres = strlen($phrase);
+$phraseMelangee = str_shuffle($phrase);
+echo 'Il y a '.$nombreDeCaractres.' caractéres dans cette phrase. ';
+echo $phraseMelangee;
+echo date('m');  */
+
+/* function direBonjour($nom)
+{
+echo'<p>Bonjour '.$nom.', ça va ?</p>';
+}
+direBonjour('Marie');
+direBonjour('Mathieu');
+direBonjour('Jobs'); */
 ?>
